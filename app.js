161357ca869396af83058ff5c74a847af3397e93
Paul Frappier <paul.frappier@hotmail.fr>
2022-11-24 15:29:22 +0100

    V5

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ app.get("/", function(req,res){
     res.sendFile(__dirname + "/index.html")
 })
 
+app.get("/CV", function(req, res){
+    res.sendFile(__dirname + "/CV.html")
+})
+
 app.listen(process.env.PORT || 3000, function(){
     console.log("The server is live on port 3000")
 })
--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,15 @@
 
-$(".toggle").click(function(){
+// $(".toggle").click(function(){
 
-    $(".slide").slideToggle("slow")
-    console.log($(".toggle").text)
+    
 
-    if($(".this").text === "See CV"){
-        $("this").text("Unshow CV")
-        console.log("workede")
-    }else{
-        $("this").text("See CV")
-    }
-})
+    // $(".slide").slideToggle("slow")
+    // console.log($(".toggle").text)
+
+    // if($(".this").text === "See CV"){
+    //     $("this").text("Unshow CV")
+    //     console.log("workede")
+    // }else{
+    //     $("this").text("See CV")
+    // }
+// })
