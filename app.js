bd6d3cbbf41c3b0c6be54657220416298ff17de6
Paul Frappier <paul.frappier@hotmail.fr>
2022-11-27 08:44:25 +0100

    improved website

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
 
 const express = require("express")
 const bodyParser = require("body-parser")
+const ejs = require("ejs")
 const app = express()
 
+app.set("view engine", "ejs")
 app.use(bodyParser.urlencoded({extended: true}))
 
 //Integrate static files (CSS, Folder images...) in your server 
 app.use(express.static("public"))
 
 app.get("/", function(req,res){
-    res.sendFile(__dirname + "/index.html")
+    res.render("index")
 })
 
-app.get("/CV", function(req, res){
-    res.sendFile(__dirname + "/CV.html")
+app.get("/cv", function(req, res){
+    res.render("cv")
 })
 
 app.listen(process.env.PORT || 3000, function(){
     console.log("The server is live on port 3000")
 })
 
+app.get("/5am-vlog", function(req, res){
+    res.render("5am.ejs")
+})
 
 
 
